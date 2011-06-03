cd2982128c5c91f229076fe4af58eeb588cefc5f
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2011-06-03 18:33:59 +0200

    put the include parameters to false, because all the js are include via the jqGridJavaScriptStackSource. With this modification, the JavaScript error disappear

--- src/main/resources/org/got5/tapestry5/jquery/jqgrid/jquery.jqgrid.core/4.0.0/src/grid.loader.js
+++ src/main/resources/org/got5/tapestry5/jquery/jqgrid/jquery.jqgrid.core/4.0.0/src/grid.loader.js
@@ -4,22 +4,22 @@ function jqGridInclude()
     var pathtojsfiles = "js/"; // need to be ajusted
     // set include to false if you do not want some modules to be included
     var modules = [
-        { include: true, incfile:'i18n/grid.locale-en.js'}, // jqGrid translation
-        { include: true, incfile:'grid.base.js'}, // jqGrid base
-        { include: true, incfile:'grid.common.js'}, // jqGrid common for editing
-        { include: true, incfile:'grid.formedit.js'}, // jqGrid Form editing
-        { include: true, incfile:'grid.inlinedit.js'}, // jqGrid inline editing
-        { include: true, incfile:'grid.celledit.js'}, // jqGrid cell editing
-        { include: true, incfile:'grid.subgrid.js'}, //jqGrid subgrid
-        { include: true, incfile:'grid.treegrid.js'}, //jqGrid treegrid
-	{ include: true, incfile:'grid.grouping.js'}, //jqGrid grouping
-        { include: true, incfile:'grid.custom.js'}, //jqGrid custom 
-        { include: true, incfile:'grid.tbltogrid.js'}, //jqGrid table to grid 
-        { include: true, incfile:'grid.import.js'}, //jqGrid import
-        { include: true, incfile:'jquery.fmatter.js'}, //jqGrid formater
-        { include: true, incfile:'JsonXml.js'}, //xmljson utils
-        { include: true, incfile:'grid.jqueryui.js'}, //jQuery UI utils
-        { include: true, incfile:'grid.filter.js'} // filter Plugin
+        { include: false, incfile:'i18n/grid.locale-en.js'}, // jqGrid translation
+        { include: false, incfile:'grid.base.js'}, // jqGrid base
+        { include: false, incfile:'grid.common.js'}, // jqGrid common for editing
+        { include: false, incfile:'grid.formedit.js'}, // jqGrid Form editing
+        { include: false, incfile:'grid.inlinedit.js'}, // jqGrid inline editing
+        { include: false, incfile:'grid.celledit.js'}, // jqGrid cell editing
+        { include: false, incfile:'grid.subgrid.js'}, //jqGrid subgrid
+        { include: false, incfile:'grid.treegrid.js'}, //jqGrid treegrid
+        { include: false, incfile:'grid.grouping.js'}, //jqGrid grouping
+        { include: false, incfile:'grid.custom.js'}, //jqGrid custom 
+        { include: false, incfile:'grid.tbltogrid.js'}, //jqGrid table to grid 
+        { include: false, incfile:'grid.import.js'}, //jqGrid import
+        { include: false, incfile:'jquery.fmatter.js'}, //jqGrid formater
+        { include: false, incfile:'JsonXml.js'}, //xmljson utils
+        { include: false, incfile:'grid.jqueryui.js'}, //jQuery UI utils
+        { include: false, incfile:'grid.filter.js'} // filter Plugin
     ];
     var filename;
     for(var i=0;i<modules.length; i++)
@@ -37,14 +37,14 @@ function jqGridInclude()
 			}
 		}
     }
-	function IncludeJavaScript(jsFile)
+    function IncludeJavaScript(jsFile)
     {
-        var oHead = document.getElementsByTagName('head')[0];
+    	var oHead = document.getElementsByTagName('head')[0];
         var oScript = document.createElement('script');
         oScript.setAttribute('type', 'text/javascript');
         oScript.setAttribute('language', 'javascript');
         oScript.setAttribute('src', jsFile);
-        oHead.appendChild(oScript);
+        //oHead.appendChild(oScript);
     }
 }
 jqGridInclude();
